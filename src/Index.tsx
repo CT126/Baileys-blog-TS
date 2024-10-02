54c9321761a6b8a4145d3bb9fb0fa9767a458f3d
CT126 <144335553+CT126@users.noreply.github.com>
2024-10-02 16:07:50 +0100

    Update Index.tsx

--- src/Index.tsx
+++ src/Index.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client'; 
-import App from './App'; 
-import 'antd/dist/reset.css'; 
-import './index.css'; 
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import { BrowserRouter } from 'react-router-dom'; 
+import 'antd/dist/reset.css';
+import './index.css';
+
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter> 
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
