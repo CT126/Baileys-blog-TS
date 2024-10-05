0d0f945e0cc9ea88524924b6d19b0e2f006af1a9
CT126 <144335553+CT126@users.noreply.github.com>
2024-10-05 21:45:07 +0100

    Update Gallery.tsx

--- src/pages/Gallery.tsx
+++ src/pages/Gallery.tsx
@@ -21,82 +21,68 @@ import baileyChilling from '../Imgs/Bailey in the garden2.jpg';
 const Gallery: React.FC = () => {
     return (
         <div className="container gallery">
-
-
-            <div className="row">
-                <div className="col-md-3">
-                    <img src={baileyAtSnowdonia} alt="Bailey at Snowdonia" />
-                    <div className="caption">Bailey at Snowdonia</div>
-                </div>
-                <div className="col-md-3">
-                    <img src={baileyAtBeach} alt="Bailey at the Beach" />
-                    <div className="caption">Bailey enjoying the beach</div>
-                </div>
-                <div className="col-md-3">
-                    <img src={babyBailey} alt="Baby Bailey" />
-                    <div className="caption">Baby Bailey</div>
-                </div>
-                <div className="col-md-3">
-                    <img src={baileyWithTeddy} alt="Bailey with a teddy" />
-                    <div className="caption">Bailey and his teddy</div>
-                </div>
+            <div className="gallery-item">
+                <img src={baileyAtSnowdonia} alt="Bailey at Snowdonia" />
+            
             </div>
-
-            <div className="row">
-                <div className="col-md-3">
-                    <img src={baileyInLivingRoom} alt="Bailey in the living room" />
-                    <div className="caption">Bailey on the sofa</div>
-                </div>
-                <div className="col-md-3">
-                    <img src={baileyNose} alt="Bailey's nose close-up" />
-                    <div className="caption">Bailey's cute nose</div>
-                </div>
-                <div className="col-md-3">
-                    <img src={baileyInSnow} alt="Bailey in the snow" />
-                    <div className="caption">Bailey playing in the snow</div>
-                </div>
-                <div className="col-md-3">
-                    <img src={adorableBailey} alt="Bailey being adorable" />
-                    <div className="caption">Adorable Bailey</div>
-                </div>
+            <div className="gallery-item">
+                <img src={baileyAtBeach} alt="Bailey at the Beach" />
+                
             </div>
-
-            <div className="row">
-                <div className="col-md-3">
-                    <img src={baileyFunnyFace} alt="Bailey funny face" />
-                    <div className="caption">Bailey pulling a funny face</div>
-                </div>
-                <div className="col-md-3">
-                    <img src={baileyWithBall} alt="Bailey with a ball" />
-                    <div className="caption">Bailey with a ball</div>
-                </div>
-                <div className="col-md-3">
-                    <img src={baileyWithHam} alt="Bailey with ham" />
-                    <div className="caption">Bailey with ham</div>
-                </div>
-                <div className="col-md-3">
-                    <img src={baileyConeOfShame} alt="Bailey with the cone of shame" />
-                    <div className="caption">Bailey with the cone</div>
-                </div>
+            <div className="gallery-item">
+                <img src={babyBailey} alt="Baby Bailey" />
+              
             </div>
-
-            <div className="row">
-                <div className="col-md-3">
-                    <img src={baileyInSun} alt="Bailey in the sun" />
-                    <div className="caption">Bailey in the sun</div>
-                </div>
-                <div className="col-md-3">
-                    <img src={baileyInGarden} alt="Bailey in the garden" />
-                    <div className="caption">Bailey in the garden</div>
-                </div>
-                <div className="col-md-3">
-                    <img src={baileyNextToWashingMachine} alt="Bailey next to the washing machine" />
-                    <div className="caption">Bailey next to the washing machine</div>
-                </div>
-                <div className="col-md-3">
-                    <img src={baileyChilling} alt="Bailey with a hat" />
-                    <div className="caption">Bailey chilling</div>
-                </div>
+            <div className="gallery-item">
+                <img src={baileyWithTeddy} alt="Bailey with a teddy" />
+             
+            </div>
+            <div className="gallery-item">
+                <img src={baileyInLivingRoom} alt="Bailey in the living room" />
+            
+            </div>
+            <div className="gallery-item">
+                <img src={baileyNose} alt="Bailey's nose close-up" />
+             
+            </div>
+            <div className="gallery-item">
+                <img src={baileyInSnow} alt="Bailey in the snow" />
+               
+            </div>
+            <div className="gallery-item">
+                <img src={adorableBailey} alt="Bailey being adorable" />
+            
+            </div>
+            <div className="gallery-item">
+                <img src={baileyFunnyFace} alt="Bailey funny face" />
+              
+            </div>
+            <div className="gallery-item">
+                <img src={baileyWithBall} alt="Bailey with a ball" />
+        
+            </div>
+            <div className="gallery-item">
+                <img src={baileyWithHam} alt="Bailey with ham" />
+        
+            </div>
+            <div className="gallery-item">
+                <img src={baileyConeOfShame} alt="Bailey with the cone of shame" />
+              
+            </div>
+            <div className="gallery-item">
+                <img src={baileyInSun} alt="Bailey in the sun" />
+              
+            </div>
+            <div className="gallery-item">
+                <img src={baileyInGarden} alt="Bailey in the garden" />
+               
+            </div>
+            <div className="gallery-item">
+                <img src={baileyNextToWashingMachine} alt="Bailey next to the washing machine" />
+          
+            </div>
+            <div className="gallery-item">
+                <img src={baileyChilling} alt="Bailey chilling" />
             </div>
         </div>
     );
